9020cd413d8eb4ab046244eff50332756e7d8b47
Roger Vera <vornadorose534@gmail.com>
2024-03-11 13:15:39 -0400

    Create Movie model, use RestTemplate to get movie data from API, and then use ObjectMapper to create a Movie object with the data we got.

--- src/main/java/com/example/demo/controller/MovieController.java
+++ src/main/java/com/example/demo/controller/MovieController.java
@@ -1,20 +1,19 @@
 package com.example.demo.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
+import com.example.demo.model.Movie;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 
 @Controller
 public class MovieController {
-
-//    private StudentService studentService;
-//
-//    public StudentController(StudentService studentService) {
-//        super();
-//        this.studentService = studentService;
-//    }
 
     @GetMapping("/search")
     public String searchMovieForm() {
@@ -26,10 +25,30 @@ public class MovieController {
     @PostMapping("/getMovie")
     // @RequestParam notation and the name of the field
     // from the form in "search_movie.html" file - which is title
-    public String getMovie(@RequestParam String title) {
+    public String getMovie(@RequestParam String title) throws JsonProcessingException {
         System.out.println("IN  MovieController->getMovie()");
         // We can now get the "title" param from the request
         System.out.println("You searched for a movie with title: " + title);
+        // Create the string we are going to use to GET the movie data using the title
+        String uri = "https://www.omdbapi.com/?apikey=b79fdda2&t=" + title;
+        // RestTemplate is a synchronous client to perform HTTP requests.
+        // Because it is synchronous, it will wait to get the response data before moving on to the next line of code.
+        RestTemplate restTemplate = new RestTemplate();
+        // getForObject() will perform a GET and return a JSON object, which we'll specify to be made a String
+        String movieDataResponse = restTemplate.getForObject(uri,String.class);
+        // We will use an ObjectMapper to deserialize the string of the JSON and create a Movie model object instance from it
+        ObjectMapper mapper = new ObjectMapper();
+        // First, we configure the mapper to ignore casing, since the response fields are capitalized,
+        mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+        // Then, we configure the mapper to ignore unknown fields in the API response, since we just want the fields
+        // we specified in our Movie model class.
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        // Now we deserialize the JSON string and make a Movie object from it.
+        Movie movie = mapper.readValue(movieDataResponse, Movie.class);
+        // We can see we now have an Object with the data.
+        System.out.println(movie.toString());
+        // Next step is to create a page that displays the movie info:
+        // TODO: More work.
         return "redirect:/search";
     }
 }
---
+++ src/main/java/com/example/demo/model/Movie.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Movie {
+    private String actors;
+    private String awards;
+    private String director;
+    private String genre;
+    private String image;
+    private String imdb_rating;
+    private String metascore;
+    private String poster;
+    private String rated;
+    private String released;
+    private String title;
+    private String writer;
+    private String year;
+
+}
